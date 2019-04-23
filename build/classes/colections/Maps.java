03963f6ddd2b514ad430a93ef8571cba24e0fa2a
Sirrine-John <46507665+Sirrine-John@users.noreply.github.com>
2019-04-22 23:16:57 -0700

    old changes

---
+++ build/classes/colections/Maps.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package colections;
+
+/**
+ *
+ * @author sirri
+ */
+public class Maps {
+    Map<Runner> runnerMap = new hashMap<>();
+    
+}
+    public void printMatch()
+}

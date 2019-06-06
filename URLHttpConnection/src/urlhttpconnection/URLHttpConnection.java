4ae685c17f0127f512a2c00d26e5d532bd650ec6
Sirrine-John <46507665+Sirrine-John@users.noreply.github.com>
2019-06-05 20:28:15 -0700

    HTTP Connection

--- URLHttpConnection/src/urlhttpconnection/URLHttpConnection.java
+++ URLHttpConnection/src/urlhttpconnection/URLHttpConnection.java
@@ -6,65 +6,77 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
+import java.io.DataOutputStream;
 
 public class URLHttpConnection  
 { 
+    private static final String USER_AGENT = "Mozilla/5.0";
     public static void main(String[] args) throws Exception
     {
     try
     {
-      String myUrl = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
-//      String myUrl = "https://www.baeldung.com/java-http-request";
-      // if your url can contain weird characters you will want to 
-      // encode it here, something like this:
-      // myUrl = URLEncoder.encode(myUrl, "UTF-8");
-      
-//      ReadJSON getJSON = new ReadJSON();
-      String results = doHttpUrlConnectionAction(myUrl);
-      //results.substring(1, results.length()-1);
-      //getJSON.reader(results);
-      System.out.println(results);
+      String getURL = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+      String postURL = "https://postman-echo.com/post/";
+     
+      String resultsGET = connectionCreate(getURL,"GET");
+      String resultsPOST = connectionCreate(postURL,"POST");
+      System.out.println(resultsGET+System.lineSeparator()+
+              "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"+System.lineSeparator()+
+              "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"+System.lineSeparator()+
+              "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"+
+              System.lineSeparator()+resultsPOST);
     }
     catch (Exception e)
     {
-      // deal with the exception in your "controller"
     }
   }
-
-  private static String doHttpUrlConnectionAction(String desiredUrl)
-  throws Exception
-  {
+    
+    private static String connectionCreate(String desiredUrl, String option)
+            throws Exception
+    {
     String requestResultMessage;
     Integer requestResultCode;
     URL url = null;
     BufferedReader reader = null;
     StringBuilder stringBuilder;
+    String inputProperties;
 
     try
     {
       // create the HttpURLConnection
       url = new URL(desiredUrl);
       HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+       connection.setRequestProperty("User-Agent", USER_AGENT);
       
-      // just want to do an HTTP GET here
-      connection.setRequestMethod("GET");
-//      connection.setRequestMethod("POST");
-//      connection.setRequestMethod("HEAD");
-//      connection.setRequestMethod("OPTIONS");
-//      connection.setRequestMethod("PUT");
-//      connection.setRequestMethod("DELETE");
-//      connection.setRequestMethod("TRACE");
-      
-      // uncomment this if you want to write output to this url
-      //connection.setDoOutput(true);
+      switch(option) {
+          case "GET":
+              connection.setRequestMethod("GET");
+              connection.setReadTimeout(15*1000);
+              connection.connect();
+              break;
+          case "POST":
+              connection.setRequestMethod("POST");
+              connection.setDoOutput(true);
+              connection.setReadTimeout(15*1000);
+              connection.connect();
+              inputProperties = "Post String 1=This is a string that shoud be retuned&Post String 2=This is the second string that should be printed";
+              try(DataOutputStream write = new DataOutputStream(connection.getOutputStream())){
+                  write.writeBytes(inputProperties);
+                  write.flush();
+              }catch(Exception e){
+                  e.printStackTrace();
+              }
+              break;
+          default:
+              System.exit(0);
+              break;
+      }
       
       // give it 15 seconds to respond
-      connection.setReadTimeout(15*1000);
-      connection.connect();
+      
       requestResultMessage = connection.getResponseMessage();
       requestResultCode = connection.getResponseCode();
-      System.out.println(requestResultCode + ": "+requestResultMessage);
+      System.out.println("Response from: "+connection.getURL().toString()+System.lineSeparator()+requestResultCode + ": "+requestResultMessage);
 
       // read the output from the server
       reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
@@ -83,20 +95,19 @@ public class URLHttpConnection
       throw e;
     }
     finally
-    {
-      // close the reader; this can throw an exception too, so
-      // wrap it in another try/catch block.
+    {// close the reader;
       if (reader != null)
       {
-        try
-        {
-          reader.close();
-        }
-        catch (IOException ioe)
-        {
-          ioe.printStackTrace();
-        }
+          try{reader.close();}
+          catch (IOException ioe){ioe.printStackTrace();}
       }
     }
-  }
+        
+    }
+//      connection.setRequestMethod("HEAD");
+//      connection.setRequestMethod("OPTIONS");
+//      connection.setRequestMethod("PUT");
+//      connection.setRequestMethod("DELETE");
+//      connection.setRequestMethod("TRACE");
+
 }
